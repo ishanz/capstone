f91c2296d52509b702a593fa6d212d4791795f76
dmt5dh <dmt5dh@virginia.edu>
2014-12-07 22:19:06 -0500

    Update sandbox.cpp

--- sandbox.cpp
+++ sandbox.cpp
@@ -11,78 +11,63 @@
 void setup();
 void loop();
 
-int sensorPin1 = 3;    // select the input pin for the potentiometer
-int sensorPin2 = 4;    // select the input pin for the potentiometer
-int sensorPin3 = 5;    // select the input pin for the potentiometer
-int ledPin = 14;      // select the pin for the LED
-int sensorValue1 = 0;  // variable to store the value coming from the sensor
-int sensorValue2 = 0;  // variable to store the value coming from the sensor
-int sensorValue3 = 0;  // variable to store the value coming from the sensor
-int previousValue = 0;
-int diffVal = 0; //difference between previous and sensor values
-int baseValue = 0;
-int flag = 0; //flag for initial start up
-int forward = 0; //flag for going forward
+int thumbPin = 3;    // select the input pin for the potentiometer
+int indexPin = 4;    // select the input pin for the potentiometer
+int midPin = 5;    // select the input pin for the potentiometer
+int clawPin = 6;
+int wristPin = 7;
+int armPin = 14;
+int thumbVal = 0;  // variable to store the value coming from the sensor
+int indexVal = 0;  // variable to store the value coming from the sensor
+int midVal = 0;  // variable to store the value coming from the sensor
+int clawVal = 0;
+int wristVal = 0;
+int armVal = 0;
+const int maxClaw = 497;
+const int maxWrist = 529;
+const int maxArm = 526;
+const int minClaw = 388;
+const int minWrist = 366;
+const int minArm = 427;
+const int maxThumb = 699;
+const int maxIndex = 678;
+const int maxMid = 572;
+const int minThumb = 648;
+const int minIndex = 627;
+const int minMid = 663;
 
 void setup() {
   // declare the ledPin as an OUTPUT:
   Serial.begin(9600);
 //  baseValue = analogRead(sensorPin);
-  pinMode(ledPin, OUTPUT);  
-  digitalWrite(6, LOW);
-  digitalWrite(7, LOW);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
+//  pinMode(ledPin, OUTPUT);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
 }
 
 void loop() {
   // read the value from the sensor:
-  sensorValue1 = analogRead(sensorPin1);
-  sensorValue2 = analogRead(sensorPin2);
-  sensorValue3 = analogRead(sensorPin3);
-  // turn the ledPin on
-  digitalWrite(ledPin, HIGH);
-//  if (flag == 0){
-//	  flag = 1;
-//  } else {
-//	  diffVal = sensorValue - previousValue;
-//  }
+  thumbVal = analogRead(thumbPin);
+  indexVal = analogRead(indexPin);
+  midVal = analogRead(midPin);
+  clawVal = analogRead(clawPin);
+  wristVal = analogRead(wristPin);
+  armVal = analogRead(armPin);
+//  digit(sensorValue);
 
+  if( (indexVal+10) > (maxIndex) && armVal > minArm){
+	  digitalWrite(9,LOW);
+	  digitalWrite(8,HIGH);
+  } else if ( (indexVal-10) < (minIndex) && armVal < maxArm ){
+	  digitalWrite(8, LOW);
+	  digitalWrite(9,HIGH);
+  } else {
+	  digitalWrite(8, LOW);
+	  digitalWrite(9, LOW);
+  }
 
-//  if ( diffVal < -20) {
-//	  digitalWrite(6, LOW);
-//	  digitalWrite(7, HIGH);
-//  } else if ( diffVal > 20) { //move down
-//	  digitalWrite(7, LOW);
-//	  digitalWrite(6, HIGH);
-//  } else { //move up
-//	  digitalWrite(7, LOW);
-//	  digitalWrite(6, LOW);
-//  }
-
-
-//  if ( (baseValue-sensorValue) < 15 ){
-//	  digitalWrite(7, LOW);
-//	  digitalWrite(6, LOW);
-//  }
-//  else if( forward == 0 && (baseValue - sensorValue) > 50){ //move down
-//	  	  forward = 1;
-//	  	digitalWrite(7, LOW);
-//		  digitalWrite(6, HIGH);
-//  } else if (forward == 1 && (baseValue - sensorValue) < 50){ //move up
-//	  forward = 0;
-//	  digitalWrite(6, LOW);
-//	  digitalWrite(7, HIGH);
-//  }
-
-  // stop the program for <sensorValue> milliseconds:
-//  delay(sensorValue);
-  // turn the ledPin off:        
-//  digitalWrite(ledPin, LOW);
-  // stop the program for for <sensorValue> milliseconds:
-//  delay(diffVal * 10);
-//  previousValue = sensorValue;
-//  Serial.println(sensorValue);
 }
 
 
